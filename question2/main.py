9e53faf2e3ed537489d32e7d68e796e7dd186aa9
Pandey-Noah-3816 <49300644+Pandey-Noah-3816@users.noreply.github.com>
2021-03-08 20:27:57 -0800

    created test1(fail) which will test nonleap year

--- question2/main.py
+++ question2/main.py
@@ -1,1 +1,11 @@
+from leapyear import*
+import unittest
 
+class TestCase(unittest.TestCase):
+  def test_case_1(self):
+    self.assertEqual (leapyear(1921),False)
+
+
+
+
+if __name__ == '__main__':unittest.main()
