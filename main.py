3322b5c702701032085d22070c88aad55db5cedb
Pandey-Noah-3816 <49300644+Pandey-Noah-3816@users.noreply.github.com>
2021-03-08 20:01:29 -0800

    added test case two which fails - will test for %3

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class TestCase(unittest.TestCase):
   #tests on volume
   def test_case_1(self):
     self.assertEqual (fizzbuzz(1),1)
+  def test_case_2(self):
+    self.assertEqual (fizzbuzz(3),'fizz')
   
   
   
