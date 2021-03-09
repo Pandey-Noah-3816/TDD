878e49c4be88b4623fc1895156890d90212232c8
Pandey-Noah-3816 <49300644+Pandey-Noah-3816@users.noreply.github.com>
2021-03-08 20:10:47 -0800

    added %3 and %5 to make test 4 pass

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,8 @@
+#Fizzbuzz
 def fizzbuzz(x):
-    if x % 3 == 0:
+    if x % 3 == 0 and x % 5 == 0:
+      return("fizzbuzz")
+    elif x % 3 == 0:
       return("fizz")
     elif x % 5 == 0:
       return("buzz")
