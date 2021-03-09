e88b0fa5f0edb14bac973475df0fb2062eaae0e5
Pandey-Noah-3816 <49300644+Pandey-Noah-3816@users.noreply.github.com>
2021-03-08 20:06:04 -0800

    added test which fails to check for %5

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from fizzbuzz import*
 import unittest
 
 class TestCase(unittest.TestCase):
-  #tests on volume
   def test_case_1(self):
     self.assertEqual (fizzbuzz(1),1)
   def test_case_2(self):
     self.assertEqual (fizzbuzz(3),'fizz')
-  
-  
-  
+  def test_case_3(self):
+    self.assertEqual (fizzbuzz(5),'buzz')
+
+
+
 if __name__ == '__main__':unittest.main()
