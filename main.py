19f6286154229f2e5707713b338e73d28f8709b2
Pandey-Noah-3816 <49300644+Pandey-Noah-3816@users.noreply.github.com>
2021-03-08 19:56:27 -0800

    test 1 fail, this will test normal output

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from fizzbuzz import*
+import unittest
 
+class TestCase(unittest.TestCase):
+  #tests on volume
+  def test_case_1(self):
+    self.assertEqual (fizzbuzz(1),1)
+  
+  
+  
+if __name__ == '__main__':unittest.main()
